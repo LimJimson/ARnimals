45c190f141be289787d7f8945d06e9d34a0eb5ba
Lim, Jimson <lim.223874@caloocan.sti.edu.ph>
2023-09-11 23:53:52 +0800

    09/11/2023 #3
    AR Optimizations
    made drop down button on AR interactions bigger

--- Assets/ARnimals/AR/Script/FrameRateManager.cs
+++ Assets/ARnimals/AR/Script/FrameRateManager.cs
@@ -7,27 +7,25 @@ public class FrameRateManager : MonoBehaviour
 int MaxRate = 9999;
 public float TargetFrameRate = 60.0f;
 float currentFrameTime;
-
 void Awake()
 {
-    QualitySettings.vSyncCount = 0;
-    Application.targetFrameRate = MaxRate;
-    currentFrameTime = Time.realtimeSinceStartup;
-    StartCoroutine("WaitForNextFrame");
+QualitySettings.vSyncCount = 0;
+Application.targetFrameRate = MaxRate;
+currentFrameTime = Time.realtimeSinceStartup;
+StartCoroutine("WaitForNextFrame");
 }
-
 IEnumerator WaitForNextFrame()
-    {
-        while (true)
-            {
-                yield return new WaitForEndOfFrame();
-                currentFrameTime += 1.0f / TargetFrameRate;
-                var t = Time.realtimeSinceStartup;
-                var sleepTime = currentFrameTime - t - 0.01f;
-                if (sleepTime > 0)
-                Thread.Sleep((int)(sleepTime * 1000));
-                while (t < currentFrameTime)
-                t = Time.realtimeSinceStartup;
-            }
-    }
+{
+while (true)
+{
+yield return new WaitForEndOfFrame();
+currentFrameTime += 1.0f / TargetFrameRate;
+var t = Time.realtimeSinceStartup;
+var sleepTime = currentFrameTime - t - 0.01f;
+if (sleepTime > 0)
+Thread.Sleep((int)(sleepTime * 1000));
+while (t < currentFrameTime)
+t = Time.realtimeSinceStartup;
+}
+}
 }

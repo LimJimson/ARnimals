a2ff4671bfbd00a8a27a6523f5bcb4b01fa74956
Lim, Jimson <lim.223874@caloocan.sti.edu.ph>
2023-09-23 11:17:16 +0800

    Revert "Revert "09/23/2023 #2""
    This reverts commit 77e66e6279f632a64079034da6bc6acbfd75f4a0.

--- Assets/ARnimals/AR/Script/showHabitat.cs
+++ Assets/ARnimals/AR/Script/showHabitat.cs
@@ -15,9 +15,9 @@ public class showHabitat : MonoBehaviour
     public ARCameraBackground ARCameraBGScript;
     
 
-    List<int> forestAnimals = new List<int>{0,1,5,6,8,17,18};
+    List<int> forestAnimals = new List<int>{0,1,5,6,8, 12, 15, 17,18};
     List<int> underWaterAnimals = new List<int>{3,11,9,13,16};
-    List<int> SavannahAnimals = new List<int>{7,2,4,10,12,14,15,19};
+    List<int> SavannahAnimals = new List<int>{7,2,4,10,14,19};
     void Awake()
     {
         modelIndex = StateNameController.animalIndexChosen;
